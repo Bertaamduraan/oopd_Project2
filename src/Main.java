5cfb83892611d60318a9989d0322a5cfa53b8bb1
Paula <paula.vivancos@students.salle.url.edu>
2026-04-07 15:45:23 +0200

    database connection done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
+import Persistence.sql.SQLConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+
+        SQLConnector db = SQLConnector.getInstance();
+
+        if (db != null) {
+            System.out.println("Connection successful!");
+
+            ResultSet rs = db.selectQuery("SELECT * FROM user");
+
+            try {
+                while (rs.next()) {
+                    // Replace with your actual column names
+                    System.out.println(rs.getString("column1") + " " + rs.getString("column2"));
+                }
+            } catch (SQLException e) {
+                System.err.println("Error reading results --> " + e.getMessage());
+            }
+
+            db.disconnect();
+
+        } else {
+            System.err.println("Connection failed!");
+        }
     }
 }
